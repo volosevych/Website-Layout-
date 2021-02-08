85a9277811e67c67e10568fd23ec252c4662980e
Anastasia Volosevych <volosevichh@gmail.com>
2021-02-08 15:50:25 -0700

    adding the new styling section and imgs

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+});
