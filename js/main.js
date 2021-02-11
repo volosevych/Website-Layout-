8bf69edb1c64862b0aa7f4dcacae2defc8881a6e
Anastasia Volosevych <volosevichh@gmail.com>
2021-02-11 11:46:45 -0700

    adding the styled news section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.slider__inner').slick({
+$('.slider__inner, .news__slider-inner').slick({
     nextArrow: '<button type="button" class="slick-btn slick-next"><i class="fas fa-chevron-right arrowIcon"></i></button>',
     prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="fas fa-chevron-left arrowIcon"></i></button>',
     infinite: false
