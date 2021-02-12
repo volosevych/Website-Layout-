3a5daa3ab3a3e52f5932e34942534b2c3d08b306
Anastasia Volosevych <volosevichh@gmail.com>
2021-02-11 19:04:52 -0700

    made header responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-$('.slider__inner, .news__slider-inner').slick({
-    nextArrow: '<button type="button" class="slick-btn slick-next"><i class="fas fa-chevron-right arrowIcon"></i></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="fas fa-chevron-left arrowIcon"></i></button>',
-    infinite: false
-});
-
-$('select').styler();
+$(function () {
+    $('.slider__inner, .news__slider-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"><i class="fas fa-chevron-right arrowIcon"></i></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="fas fa-chevron-left arrowIcon"></i></button>',
+        infinite: false
+    });
+    
+    $('.header__btn-menu').on('click', function() {
+        $('.menu ul').slideToggle();
+    })
+    
+    $('select').styler();
+})
